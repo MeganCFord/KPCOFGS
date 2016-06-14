d44046d729c4d9f9e63513e84f33645526d0b087
Megan Ford <meg.c.ford@gmail.com>
2016-06-14 10:57:10 -0500

    API URL console logs correctly

--- src/app/main.js
+++ src/app/main.js
@@ -10,10 +10,38 @@ angular.module("Ident", ["ngRoute"])
       .otherwise("/");
   })
 
-  .controller("Identer", function($scope) {
+  .controller("Identer", function($scope, COLFactory) {
 
     const identer = this;
 
     $scope.title="hello world (angular)";
 
+    COLFactory.getAPIURL();
+
+  })
+
+  .factory("COLFactory", function($http) {
+
+    return {
+      //tried animalia and did not get child taxa...
+    
+      getAPIURL: () => {
+        return  $http({
+          method: "GET", 
+          url: "http://www.catalogueoflife.org/col/webservice?name=Animalia&format=json&response=full"
+        }).then( (res) => {
+          console.log("response", res.data );
+        }, (e) => {
+          console.log("error", e );
+        });
+      }
+    };//end of return
   });
+
+
+const breakup = function() {
+  let relationship = ["Megan", "Michael"];
+  relationship.splice("Michael", 1);
+  relationship.push("loneliness");
+  return relationship;
+};
