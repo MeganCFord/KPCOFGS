db0662bb5a1b295be3ac660243641fb318f27218
Megan Ford <meg.c.ford@gmail.com>
2016-06-14 11:21:13 -0500

    tree traversal links work down to genus level.

--- src/app/main.js
+++ src/app/main.js
@@ -13,35 +13,54 @@ angular.module("Ident", ["ngRoute"])
   .controller("Identer", function($scope, COLFactory) {
 
     const identer = this;
+    identer.currentTaxa = {};
 
-    $scope.title="hello world (angular)";
+    identer.title="hello world (angular)";
 
-    COLFactory.getAPIURL();
+    COLFactory.startPage().then((data) => {identer.currentTaxa = data; console.log("identer taxa", identer.currentTaxa);});
 
+    identer.sendNewTaxa = (nameToSend) => {
+      console.log("url to send", nameToSend );
+      COLFactory.sendNewTaxa(nameToSend).then((data) => {identer.currentTaxa = data; console.log("new identer taxa", identer.currentTaxa);});
+    };
   })
 
   .factory("COLFactory", function($http) {
 
+    let currentTaxa = {};
+
     return {
-      //tried animalia and did not get child taxa...
-    
-      getAPIURL: () => {
+
+  //for higher taxa, response=full is required. 
+  //response data: results[0].name, results[0].url, results[0].child_taxa is an array of objects.     
+      startPage: () => {
         return  $http({
           method: "GET", 
-          url: "http://www.catalogueoflife.org/col/webservice?name=Animalia&format=json&response=full"
+          url: "http://www.catalogueoflife.org/col/webservice?name=Chordata&format=json&response=full"
         }).then( (res) => {
-          console.log("response", res.data );
+          currentTaxa = res.data.results[0];
+          return currentTaxa;
         }, (e) => {
           console.log("error", e );
         });
+      }, //end of getAPIURL
+
+      sendNewTaxa: (nameToSend) => {
+        return  $http({
+          method: "GET", 
+          url: `http://www.catalogueoflife.org/col/webservice?name=${nameToSend}&format=json&response=full`
+        }).then( (res) => {
+          currentTaxa = res.data.results[0];
+          console.log("current taxa data", currentTaxa );
+          return currentTaxa;
+        }, (e) => {
+          console.log("error", e );
+        });
+      }, //end of sendNewTaxa
+
+      //just in case. TODO: delete this if unused.
+      getCurrentTaxa: () => {
+        return currentTaxa;
       }
     };//end of return
   });
-
-
-const breakup = function() {
-  let relationship = ["Megan", "Michael"];
-  relationship.splice("Michael", 1);
-  relationship.push("loneliness");
-  return relationship;
-};
