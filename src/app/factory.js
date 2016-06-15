a0103aee00390fd6a1ac875f7cf99d919842b243
Megan Ford <meg.c.ford@gmail.com>
2016-06-15 14:41:13 -0500

    switched get info functions to click event

--- src/app/factory.js
+++ src/app/factory.js
@@ -1,10 +1,6 @@
 angular.module("Ident")
   .factory("COLFactory", function($http) {
 
-    let currentCommonInfo = [];
-
-
-    //pulled the function out of the return statement so I could put other functions into it. 
     function COLforTaxa (nameToSend) {
       return $http({
         method: "GET", 
@@ -21,36 +17,65 @@ angular.module("Ident")
     }//end of COLforTaxa
 
 
-    function EOLforID (nameToSend) {
+    function populateTaxaCard (nameToSend) {
         return $http({
           method: "GET", 
           url: `http://eol.org/api/search/1.0.json?q=${nameToSend}&page=1&exact=true&filter_by_taxon_concept_id=&filter_by_hierarchy_entry_id=&filter_by_string=&cache_ttl=`
         }).then((res) => {
-          return res.data.results[0].id;
+          return (res.data.results[0].id? res.data.results[0].id : res.data);
         }, (e) => {
           console.log("error", e );
-        })
+        })//end of resolve/reject
         .then((currentEolId)=> {
           return EOLforCommonInfo(currentEolId);
-        });
+        })
+        .then((data) => parseCommonInfo(data));//end of .then
     }//end of EOL for Id
 
 
     function EOLforCommonInfo(idToSend) {
       return $http({
         method: "GET", 
-        url: `http://eol.org/api/pages/1.0.json?batch=false&id=${idToSend}&images_per_page=1&images_page=1&videos_per_page=0&videos_page=0&sounds_per_page=0&sounds_page=0&maps_per_page=0&maps_page=0&texts_per_page=2&texts_page=1&iucn=false&subjects=overview&licenses=all&details=true&common_names=true&synonyms=true&references=false&taxonomy=false&vetted=0&cache_ttl=&language=en`
+        url: `http://eol.org/api/pages/1.0.json?batch=false&id=${idToSend}&images_per_page=10&images_page=1&videos_per_page=0&videos_page=0&sounds_per_page=0&sounds_page=0&maps_per_page=0&maps_page=0&texts_per_page=2&texts_page=1&iucn=false&subjects=overview&licenses=all&details=true&common_names=true&synonyms=true&references=false&taxonomy=false&vetted=0&cache_ttl=&language=en`
       }).then((res) => {
         return res.data;
       }, (e) => {
         console.log("error", e );
-      });
-    }
-    return {
+      })//end of resolve/reject
+    }//end of EOLforCommonInfo
 
+    function parseCommonInfo(subtaxa) {
+      return new Promise(function(resolve, reject) {
+      // console.log("parser is starting" );
+        // console.log("object", subtaxa);
+        subtaxa.textStuff = [];
+        subtaxa.pictures = [];
+
+        subtaxa.vernacularNames.forEach((name) => {
+          if(name.language === "en" && name.eol_preferred=== true) {
+            subtaxa.commonName = name.vernacularName;
+          }//end of if-else
+        });//end of vernacularname forEach 
+
+        subtaxa.dataObjects.forEach((object) => {
+          console.log("running data objects loop", object );
+          if (object.mimeType === "text/plain" || object.mimeType === "text/html") {
+            subtaxa.textStuff.push( object.description);
+          } 
+          if (object.mimeType === "image/jpeg") {
+            subtaxa.pictures.push( object.mediaURL);
+          }//end of if-else
+        });//end of dataObjects forEach
+        
+        resolve(subtaxa);
+      });//end of promise
+    }//end of parseCommonInfo
+
+    return {
       COLforTaxa: COLforTaxa, 
-      EOLforID: EOLforID, 
-      EOLforCommonInfo: EOLforCommonInfo
-     
+      populateTaxaCard: populateTaxaCard, 
+      EOLforCommonInfo: EOLforCommonInfo,  
+      parseCommonInfo: parseCommonInfo
     };//end of return
+
   }); //end of factory
--- src/app/main.js
+++ src/app/main.js
@@ -10,28 +10,34 @@ angular.module("Ident", ["ngRoute", "ui.bootstrap"])
       .otherwise("/");
   })
 
-  .controller("Identer", function($scope, COLFactory) {
-
+  .controller("Identer", function(COLFactory, $scope, $sce, $timeout) {
     const identer = this;
-    identer.currentTaxa = {};
-    identer.subTaxa = [];
 
-    identer.title= identer.currentTaxa.name;
+    
+    $scope.renderHtml = function(code) {
+      return $sce.trustAsHtml(code);
+    };
 
-//gets the current taxa, then runs through all the subtaxa to get all their info. it does NOT get the vernacular info of the current taxa though. 
+//gets the current taxa with scientific names only of child taxa. add'l info loads on a click. 
     identer.setUpPage = (nameToSend) => {
       identer.subTaxa = [];
       COLFactory.COLforTaxa(nameToSend)
-      .then((data) => {identer.currentTaxa = data; console.log("current taxa data", identer.currentTaxa );})
-      .then(() => {
-        return Promise.all(identer.currentTaxa.child_taxa.map(function (taxa) {
-          return COLFactory.EOLforID(taxa.name);
-        }));
-      }).then((subtaxaInfo) => {
-        identer.subTaxa =subtaxaInfo;
-        console.log("array of subtaxa info", subtaxaInfo );
-      });//end of .then
+      
+      .then((data) => {
+        identer.currentTaxa = data; 
+        console.log("current taxa data", identer.currentTaxa );
+      });
     };//end of setUpPage
+
+
+    identer.populateTaxaCard = (scientificName) => {
+      COLFactory.populateTaxaCard(scientificName)
+      .then((info) => {
+        identer.modalInfo = info;
+        console.log("identer modal info", identer.modalInfo );
+      });
+    };
+
     //run this on start, perhaps as a resolve or something? 
     identer.setUpPage("Reptilia");
   });
