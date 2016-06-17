9957173ca190f0fe80a6ea23c7fc85ff9a314724
Megan Ford <meg.c.ford@gmail.com>
2016-06-17 15:08:18 -0500

    go back button works. started setting up cumulative 'my animal' text.

--- src/app/tree/COLFactory.js
+++ src/app/tree/COLFactory.js
@@ -16,8 +16,10 @@ angular.module("Ident")
         
         //species objects don't have a 'results' object. 
         //TODO: set a 'isSpecies' variable to true or false based on this ternary as well.
-        return (res.data.results ? 
-           res.data.results[0] : res.data);
+        currentTaxaData = res.data.results ? 
+           res.data.results[0] : res.data;
+        currentTaxaData = addMyOwnData(currentTaxaData);
+        return currentTaxaData;
       
       }, (e) => {
         console.err(e);
@@ -25,9 +27,10 @@ angular.module("Ident")
       }).then((data) => {
         //see above re: only doing this for higher taxa. This function populates the 'questions' on the main page to aid with tree traversal.
         data.child_taxa.forEach((taxa) => {
-          taxa = checkScientificName(taxa);
+          taxa = addMyOwnData(taxa);
         });
         currentTaxaData = data;
+        console.log("current taxa data", currentTaxaData );
         return data;
       });//end of .then. 
     }//end of COLforTaxa
@@ -36,64 +39,67 @@ angular.module("Ident")
 
       //TODO: move this data into firebase, use a Promise.all. 
       
-    function checkScientificName(taxa) {
+    function addMyOwnData(taxa) {
       const currentTaxa = taxa;
       switch(taxa.name) {
+      case "Chordata":
+        currentTaxa.question = "is a creature with a spinal cord (and probably a spine).";
+        break;
         //CHORDATA SUBTAXA
       case "Actinopterygii":
-        currentTaxa.question = "My animal is a fish with fins that are webs of skin supported by bony spines.";
+        currentTaxa.question = "is a fish with fins that are webs of skin supported by bony spines.";
         currentTaxa.enableMe=false;
         break;
       case "Amphibia": 
-        currentTaxa.question = "My animal is probably an amphibian: it has slimy skin with no scales or fur/hair, and goes through a metamorphosis from an egg laid in water to adult form.";
+        currentTaxa.question = "is probably an amphibian: it has slimy skin with no scales or fur/hair, and goes through a metamorphosis from an egg laid in water to adult form.";
         currentTaxa.enableMe=false;
         break;
       case "Appendicularia":
-        currentTaxa.question = "My animal is a tiny clear plankton that usually floats near the ocean's surface and filters seawater through a sac to eat.";
+        currentTaxa.question = "is a tiny clear plankton that usually floats near the ocean's surface and filters seawater through a sac to eat.";
         currentTaxa.enableMe=false;
         break;
       case "Ascidiacea":
-        currentTaxa.question = "My animal is attached to a rock in the ocean and filters seawater through a sac to eat.";
+        currentTaxa.question = "is attached to a rock in the ocean and filters seawater through a sac to eat.";
         currentTaxa.enableMe=false;
         break;
       case "Aves":
-        currentTaxa.question = "My animal has feathers and wings.";
+        currentTaxa.question = "has feathers and wings.";
         currentTaxa.enableMe=false;
         break;
       case "Cephalaspidomorphi":
-        currentTaxa.question = "My animal is fish with a sucker mouth and no jaw.";
+        currentTaxa.question = "is fish with a sucker mouth and no jaw.";
         currentTaxa.enableMe=false;
         break;
       case "Elasmobranchii":
-        currentTaxa.question = "My animal is a fish with cartilage instead of bones and a bony jaw separate from its skull- probably a shark or ray.";
+        currentTaxa.question = "is a fish with cartilage instead of bones and a bony jaw separate from its skull- probably a shark or ray.";
         currentTaxa.enableMe=false;
         break;
       case "Holocephali":
-        currentTaxa.question = "My animal is a fish with cartilage instead of bones, a long, whiplike tail, and a small, fleshy mouth." ;
+        currentTaxa.question = "is a fish with cartilage instead of bones, a long, whiplike tail, and a small, fleshy mouth." ;
         currentTaxa.enableMe=false;
         break;
       case "Leptocardii":
-        currentTaxa.question = "My animal is a 2-3 inch long worm-shaped fish with no fins, a poorly-shaped tail, and only a little cartilage stiffening its gills.";
+        currentTaxa.question = "is a 2-3 inch long worm-shaped fish with no fins, a poorly-shaped tail, and only a little cartilage stiffening its gills.";
         currentTaxa.enableMe=false;
         break;
       case "Mammalia": 
-        currentTaxa.question = "My animal has fur or hair and gives birth to its babies.";
+        currentTaxa.question = "has fur or hair and gives birth to its babies.";
         currentTaxa.enableMe=true;
         break;
       case "Myxini": 
-        currentTaxa.question = "My animal is a slimy eel-shaped fish with eye spots, a cartilaginous skull, one nostril but no spinal bones.";
+        currentTaxa.question = "is a slimy eel-shaped fish with eye spots, a cartilaginous skull, one nostril but no spinal bones.";
         currentTaxa.enableMe=false;
         break;
       case "Reptilia":
-        currentTaxa.question = "My animal is a reptile, so it's probably dry and has scales.";
+        currentTaxa.question = "is a reptile, so it's probably dry and has scales.";
         currentTaxa.enableMe=true;
         break;
       case "Sarcopterygii":
-        currentTaxa.question = "My animal is a bony fish with lobed fins attached to the body by a single bone.";
+        currentTaxa.question = "is a bony fish with lobed fins attached to the body by a single bone.";
           currentTaxa.enableMe=true;
         break;
       case "Thaliacea":
-        currentTaxa.question = "My animal is a small, free-floating sac-like creature that filters seawater for food and propulsion.";
+        currentTaxa.question = "is a small, free-floating sac-like creature that filters seawater for food and propulsion.";
         currentTaxa.enableMe=false;
         break;
       default: 
