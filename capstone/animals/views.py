9161e6cf0c4550df8523506adb2ca1169e3c7a5c
Megan Ford <meg.c.ford@gmail.com>
2016-09-01 10:34:16 -0500

    moved all the stuff into django, got a django hello world.

---
+++ capstone/animals/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from django.conf.urls.static import static
+
+# Create your views here.
+
+class IndexView(generic.TemplateView):
+  template_name = 'animals/index.html'
